abde31e7e6a1b8a54e1625f3e003570606488765
tranhaidang2 <tranhaidangq2003@gamil.com>
2024-12-03 23:52:54 +0700

    Feat: DiseaseController->Add->Details

--- ShrimpDiseasePrevention/Controllers/DiseaseController.cs
+++ ShrimpDiseasePrevention/Controllers/DiseaseController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ShrimpDiseasePrevention.Models;
+using ShrimpDiseasePrevention.ViewModels;
 
 namespace ShrimpDiseasePrevention.Controllers
 {
@@ -21,13 +22,138 @@ namespace ShrimpDiseasePrevention.Controllers
                 .Include(d => d.User)
                 .ToListAsync();
 
-            if (diseaseList == null)
-            {
-                diseaseList = new List<Disease>();
-            }
-
             ViewBag.Diseases = diseaseList;
             return View();
         }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var diseaseDetails = await _context.Diseases
+                .Include(d => d.Preventions)
+                .Include(d => d.Images)
+                .Include(d => d.User)
+                .FirstOrDefaultAsync(n => n.DiseaseId == id);
+
+            if (diseaseDetails == null)
+            {
+                return NotFound();
+            }
+
+            ViewBag.DiseaseDetails = diseaseDetails;
+
+            ViewBag.Images = diseaseDetails.Images;
+
+            ViewBag.UserFullName = diseaseDetails.User?.UserFullName;
+
+            return View();
+        }
+
+        public IActionResult AddDisease()
+        {
+            var userId = HttpContext.Session.GetInt32("UserId");
+
+            if (userId == null) 
+            {
+                return RedirectToAction("Login", "Access");
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddDisease(DiseaseViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var userId = HttpContext.Session.GetInt32("UserId");
+
+                var disease = new Disease
+                {
+                    DiseaseTitle = model.DiseaseTitle,
+                    DiseaseContent = model.DiseaseContent,
+                    DiseaseShortDescription = model.DiseaseShortDescription,
+                    DiseaseSymptoms = model.DiseaseSymptoms,
+                    DiseaseCreateAt = DateTime.Now,
+                    UserId = userId,
+                };
+
+                await _context.Diseases.AddAsync(disease);
+                await _context.SaveChangesAsync();
+
+                var diseaseImageDirectory = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "DiseaseImage", disease.DiseaseId.ToString());
+                if (!Directory.Exists(diseaseImageDirectory))
+                {
+                    Directory.CreateDirectory(diseaseImageDirectory);
+                }
+
+                if (model.DiseaseImageFiles != null && model.DiseaseImageFiles.Any())
+                {
+                    foreach (var file in model.DiseaseImageFiles)
+                    {
+                        var fileName = $"{disease.DiseaseId}_{file.FileName}";
+                        var filePath = Path.Combine(diseaseImageDirectory, fileName);
+
+                        using (var stream = new FileStream(filePath, FileMode.Create))
+                        {
+                            await file.CopyToAsync(stream);
+                        }
+
+                        var image = new Image
+                        {
+                            DiseaseId = disease.DiseaseId,
+                            ImagePath = $"/DiseaseImage/{disease.DiseaseId}/{fileName}",
+                            ImageCreateAt = DateTime.Now,
+                        };
+
+                        _context.Images.Add(image);
+                    }
+                }
+
+                var prevention = new Prevention
+                {
+                    PreventionContent = model.PreventionContent,
+                };
+
+                await _context.Preventions.AddAsync(prevention);
+                await _context.SaveChangesAsync();
+
+                disease.Preventions.Add(prevention);
+
+                var preventionImageDirectory = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "PreventionImage", disease.DiseaseId.ToString());
+                if (!Directory.Exists(preventionImageDirectory))
+                {
+                    Directory.CreateDirectory(preventionImageDirectory);
+                }
+
+                if (model.PreventionImageFiles != null && model.PreventionImageFiles.Any())
+                {
+                    foreach (var file in model.PreventionImageFiles)
+                    {
+                        var fileName = $"{disease.DiseaseId}_{file.FileName}";
+                        var filePath = Path.Combine(preventionImageDirectory, fileName);
+
+                        using (var stream = new FileStream(filePath, FileMode.Create))
+                        {
+                            await file.CopyToAsync(stream);
+                        }
+
+                        var image = new Image
+                        {
+                            PreventionId = prevention.PreventionId,
+                            ImagePath = $"/PreventionImage/{disease.DiseaseId}/{fileName}",
+                            ImageCreateAt = DateTime.Now,
+                        };
+
+                        await _context.Images.AddAsync(image);
+                    }
+                }
+
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
     }
 }
---
+++ ShrimpDiseasePrevention/ViewModel/DiseaseViewModel.cs
@@ -0,0 +1,27 @@
+﻿namespace ShrimpDiseasePrevention.ViewModels
+{
+    public class DiseaseViewModel
+    {
+        public int DiseaseId { get; set; }
+
+        public string DiseaseTitle { get; set; } = null!;
+
+        public string DiseaseContent { get; set; } = null!;
+
+        public string? DiseaseShortDescription { get; set; }
+
+        public string? DiseaseSymptoms { get; set; }
+
+        public DateTime? DiseaseCreateAt { get; set; }
+
+        public int? UserId { get; internal set; }
+
+        public string? UserFullName { get; set; }
+
+        public string? PreventionContent { get; set; }
+
+        public IEnumerable<IFormFile>? DiseaseImageFiles { get; set; }
+
+        public IEnumerable<IFormFile>? PreventionImageFiles { get; set; }
+    }
+}
